ac1f41377870b21b236cf12946562ea6316c7a77
Aditya Kumar <adikid1996@gmail.com>
2021-03-19 21:25:10 +0530

    testing auto deploys #heroku_cd
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

--- src/main/java/com/projectsparetime/pivotserver/WelcomeController.java
+++ src/main/java/com/projectsparetime/pivotserver/WelcomeController.java
@@ -9,4 +9,9 @@ public class WelcomeController {
     public String welcome() {
         return "welcome to pivot server";
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello endpoint is working";
+    }
 }
